41cd3a55bb8b53b1505561e940a47fc44be05942
Daniel Harrison <harrison92d@gmail.com>
2022-06-23 08:29:00 +0100

    Add infrastructure

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,51 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 3.10.0"
+      }
+    }
+  
+    backend "azurerm" {
+      resource_group_name = "rg-terraform-state"
+      storage_account_name = "dhterraform"
+      container_name = "tfstate"
+      key = "prod.terraform.tfstate"
+    }
+  
+    required_version = ">= 1.1.0"
+  }
+  
+  provider "azurerm" {
+    features {}
+  }
+  
+  resource "azurerm_resource_group" "dhexpressapp" {
+    name     = "dhexpressapp"
+    location = "uksouth"
+  }
+  
+  resource "azurerm_service_plan" "dhexpressapp" {
+    name = "dhexpressapp"
+    resource_group_name = azurerm_resource_group.dhexpressapp.name
+    location = azurerm_resource_group.dhexpressapp.location
+    sku_name = "B1"
+    os_type = "Linux"
+  }
+  
+  resource "azurerm_linux_web_app" "dhexpressapp" {
+    name = "dhexpressapp"
+    resource_group_name = azurerm_resource_group.dhexpressapp.name
+    location = azurerm_resource_group.dhexpressapp.location
+    service_plan_id = azurerm_service_plan.dhexpressapp.id
+  
+    app_settings = {
+      "PORT" = "80"
+    }
+  
+    site_config {
+      application_stack {
+        node_version = "16-lts"
+      }
+    }
+  }
